71e59c9769bcccc0456eb6942a491b045874b197
SanduniWijesingheP <sanduniwijesinghe9792>
2021-10-09 09:45:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This project is Customer save and Item save to the database.
+
+app run
+    setup gradle and run
+    
+app build
+    gradle build
+    
+deployment instructions
+    build and get jar then deploy
---
+++ src/DbConnection.java
@@ -0,0 +1,27 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/ShoppingS",
+                "root",
+                "password");
+    }
+    public static DbConnection getInstance() throws ClassNotFoundException, SQLException {
+        if (dbConnection==null){
+            dbConnection= new DbConnection();
+        }
+        return dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
