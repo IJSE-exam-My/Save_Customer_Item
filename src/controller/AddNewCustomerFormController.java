58229b6a59efe064d3e84530eb47345b28346a18
SanduniWijesingheP <sanduniwijesinghe9792>
2021-10-09 12:43:30 +0530

    Create Customer Section close #1

--- src/controller/AddNewCustomerFormController.java
+++ src/controller/AddNewCustomerFormController.java
@@ -16,14 +16,14 @@ public class AddNewCustomerFormController {
     public JFXTextField txtCity;
     public JFXTextField txtProvince;
 
-    public void addNewCustomerOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+    public void addNewCustomerOnAction  (ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         Customer c1=new Customer
                 (txtID.getText(),txtTitle.getText(),txtName.getText(),txtAddress.getText(),
                 txtCity.getText(),txtProvince.getText(),txtPostCode.getText());
 
-        if(new lk.ijse.pos_system.controller.CustomerController().saveCustomer(c1))
+        if   (new lk.ijse.pos_system.controller.CustomerController().saveCustomer(c1))
             new Alert(Alert.AlertType.CONFIRMATION, "Saved..").show();
         else
-            new Alert(Alert.AlertType.WARNING, "Try Again..").show();
+            new Alert  (Alert.AlertType.WARNING, "Try Again..").show();
     }
 }
